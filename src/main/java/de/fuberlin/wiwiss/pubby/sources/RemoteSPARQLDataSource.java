48c602376481fcfe84857eb746a71d2f01746021
Timo Homburg <timo.homburg@hs-mainz.de>
2020-05-26 17:22:44 +0200

    Update RemoteSPARQLDataSource.java

--- src/main/java/de/fuberlin/wiwiss/pubby/sources/RemoteSPARQLDataSource.java
+++ src/main/java/de/fuberlin/wiwiss/pubby/sources/RemoteSPARQLDataSource.java
@@ -74,7 +74,7 @@ public class RemoteSPARQLDataSource implements DataSource {
 				new HashSet<String>(Arrays.asList(new String[]{
 					"CONSTRUCT {?__this__ ?p ?o} WHERE {?__this__ ?p ?o. FILTER (?p NOT IN ?__high_outdegree_properties__)}",
 					"CONSTRUCT {?s ?p ?__this__} WHERE {?s ?p ?__this__. FILTER (?p NOT IN ?__high_indegree_properties__)}",
-					"CONSTRUCT {?p ?o ?__this__} WHERE {?abc <http://www.opengis.net/ont/geosparql#hasGeometry> ?__this__ . ?__this__ ?p ?o . "
+					"CONSTRUCT {?p ?o ?__this__} WHERE {?__this__ <http://www.opengis.net/ont/geosparql#hasGeometry> ?geo . ?geo ?p ?o . "
 				})) :
 				Collections.singleton("DESCRIBE ?__this__");
 		}
